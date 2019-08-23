c5096408112b2c0ba7c3f4639ec049d3cca155fb
Kleanthis Tsesmetzidis <ktsesmetzidis@gmail.com>
2019-08-24 02:17:02 +0300

    Sign up - Create
    - Create Customer, Staff
    - Use the dependency with the Account

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,24 @@
 const router = require("express").Router();
 const jwt = require("jsonwebtoken");
 
+const {staff, customer} = require('../models');
+
+const create = async (req, res, next) => {
+    try {
+        let newOne = (true)
+            ? await customer.create(req.body)
+            : await staff.create(req.body)
+        newOne.setAccount(req.user);
+        next()
+    } catch (err) {
+        next(err)
+    }
+};
+
 module.exports = passport => {
 
-    router.post('/signup', passport.authenticate('signup', {session: false}), async (req, res) => {
-        res.json('elaPousai')
+    router.post('/signup', passport.authenticate('signup', {session: false}), create, async (req, res) => {
+        res.json(req.user.username + ' created')
     });
 
     router.post('/login', async (req, res, next) =>
@@ -15,7 +29,11 @@ module.exports = passport => {
                 req.login(account, {session: false}, async (err) => {
                     if (err) return next(err);
 
-                    const body = {_id: account.id, username: account.username};
+                    const person = (true)
+                        ? await customer.findOne({where: {accountId: account.id}})
+                        : await staff.findOne({where: {accountId: account.id}})
+
+                    const body = {_id: account.id, username: account.username, person};
                     const token = jwt.sign(body, 'top_secret', {expiresIn: '1h'});
                     return res.json({token});
                 });
