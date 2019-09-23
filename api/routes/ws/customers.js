e4befaff009ea76207c5b3ed0aaa2ad634a209c3
Kleanthis Tsesmetzidis <ktsesmetzidis@gmail.com>
2019-09-23 22:36:51 +0300

    Refine socket.io
    - Use of the authorizer on each of namespaces
    - Require each namespace as a module
    - Refine index.js

--- api/routes/ws/customers.js
+++ api/routes/ws/customers.js
@@ -1,11 +1,18 @@
-module.exports = (io, socket) => {
+module.exports = io =>
 
-    let currentRoom = socket.handshake.query.token;
+    io
+        .of('customers')
+        .use(require('socketio-jwt').authorize({
+            secret: 'top_secret',
+            handshake: true
+        }))
+        .on('connection', socket => {
+            let currentRoom = socket.handshake.query.token;
 
-    socket.join(currentRoom);
-    socket.on('message', msg => {
-        socket.emit('hey', {msg: msg, answer: true})
-        socket.in(currentRoom).emit('roomie', {msg: msg, intoRoom: currentRoom});
-        io.of('customers').emit('broadcast', {OLOI: "MAZI-CUSTOMERS"});
-    })
-};
+            socket.join(currentRoom);
+            socket.on('message', msg => {
+                socket.emit('hey', {msg: msg, answer: true})
+                socket.in(currentRoom).emit('roomie', {msg: msg, intoRoom: currentRoom});
+                io.of('customers').emit('broadcast', {OLOI: "MAZI-CUSTOMERS"});
+            })
+        });
--- api/routes/ws/hotels.js
+++ api/routes/ws/hotels.js
@@ -1,12 +1,26 @@
-module.exports = (io, socket) => {
+module.exports = io =>
 
-    let currentRoom = socket.handshake.query.token;
+    io
+        .of('hotels')
+        .use(require('socketio-jwt').authorize({
+            secret: 'top_secret',
+            handshake: true
+        }))
+        .on('connection', socket => {
+            let currentRoom = socket.handshake.query.token;
 
-    socket.join(currentRoom);
-    socket.on('message', msg => {
-        socket.emit('hey', {msg: msg, answer: true})
-        socket.in(currentRoom).emit('roomie', {msg: msg, intoRoom: currentRoom});
-        io.of('hotels').emit('broadcast', {OLOI: "MAZI"});
-        io.of('customers').emit('broadcast', {OLOI: "MAZI"});
-    })
-};
+            socket.join(currentRoom);
+            socket.on('message', msg => {
+                socket.emit('hey', {msg: msg, answer: true})
+                socket.in(currentRoom).emit('roomie', {msg: msg, intoRoom: currentRoom});
+                // todo check io.nsps to broadcast to all clients, even if they are connected to other namespace
+                io.of('hotels').emit('broadcast', {OLOI: "MAZI"});
+                io.of('customers').emit('broadcast', {OLOI: "MAZI"});
+            });
+            socket.on('assign', msg => {
+                console.log(socket.conn.pingTimeoutTimer._idleStart)
+                console.log(socket.decoded_token)
+                socket.emit('hey', {msg: msg, answer: true, token: socket.handshake.query.token, rooms: socket.rooms})
+
+            })
+        });
