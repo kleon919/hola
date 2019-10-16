486d18a927ae53a8b51dabcb7977296ee9e0c784
Kleanthis Tsesmetzidis <ktsesmetzidis@gmail.com>
2019-10-16 21:47:19 +0300

    Update CORS
    - Always response with 200 to OPTIONS request

--- middle/cors.js
+++ middle/cors.js
@@ -3,5 +3,8 @@ module.exports = (req, res, next) => {
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Authorization, Content-Type");
     res.header("Access-Control-Allow-Credentials", true);
+
+    ('OPTIONS' == req.method) && res.send(200);
+
     next();
 };
