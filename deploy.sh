eff6024ed08032addbab826da3768966617146ff
Kleanthis Tsesmetzidis <ktsesmetzidis@gmail.com>
2019-10-29 18:17:44 +0200

    Refine deploy
    - Use Alpine linux as build dist
    - Refine deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 
 NAME="hola_api"
+TMP_DIR="./tmp/hola"
 
-tmp_dir="./tmp/hola"
-rm -rf $tmp_dir
+rm -rf $TMP_DIR
 
 # Git Clone, credentials
 git config --global credential.helper 'cache --timeout=3600'
-git clone --branch develop https://github.com/kleon919/hola.git $tmp_dir
+git clone --branch develop https://github.com/kleon919/hola.git $TMP_DIR
 
 # Remove container if exists
 CONT=$(docker ps -a -q  --filter "name=$NAME")
 
 if [ "$CONT" != "" ]
 then
-    echo "Container Exist"
     docker rm -f $CONT
-else
-   echo "Container does not exist"
 fi
 
-cp ./config.json $tmp_dir/config/
-cd $tmp_dir
+cp ./config.json $TMP_DIR/config/
+cd $TMP_DIR
 
-sudo docker build -t $NAME .
-sudo docker run --name $NAME -p 8000:8000 -d $NAME
+docker build -t $NAME .
+docker run --name $NAME -p 8000:8000 -d $NAME
 
 docker rmi $(docker images -f "dangling=true" -q)
 # IMAGE=$(docker images $NAME --format "{{.ID}}")
