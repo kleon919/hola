b199f470396b98d16afc0bbf8febbf4028a36f98
Kleanthis Tsesmetzidis <ktsesmetzidis@gmail.com>
2020-01-26 11:54:42 +0200

    Refactor ML model
    - Update classes and their names
    - Create a normalization function
    - Use it against to each value

--- core/tf/index.js
+++ core/tf/index.js
@@ -7,21 +7,43 @@ const init = async () => {
 };
 
 const choices = [
-    "Pasta-Bolognese (Soy)",
-    "Chef Salad",
-    "Greek Salad",
     "Club Sandwich",
-    "Pasta-Bolognese",
-    "Sushi",
     "Steak",
-    "Burger and Fries"
+    "Burger",
+    "Chef Salad",
+    "Pasta",
+    "French Fries",
+    "Greek Salad",
+    "Boiled Vegetables"
 ];
+
+const normalize = (value, min, max) => {
+
+    value = (value < min) ? min : value;
+    value = (value > max) ? max : value;
+
+    return (value - min) / (max - min);
+
+};
+
+const normalizeAge = age => normalize(age, 15, 75);
+
+const normalizeTime = secs => normalize(secs, 720, 1320);
+
+const normalizeVegeterian = value => normalize(value, 1, 10);
 
 const maxIndex = arr => arr.indexOf(Math.max(...arr));
 
-// Input must be an array with 3 values
+// input : Array [3], [AGE, VEGETERIAN, CURRENT_TIME]
 const predict = async input => {
-    let res = await model.predict(tf.tensor([input])).array();
+
+    let normalInput = [
+        normalizeAge(input[0]),
+        normalizeVegeterian(input[1]),
+        normalizeTime(input[2])
+    ];
+
+    let res = await model.predict(tf.tensor([normalInput])).array();
     return choices[maxIndex(...res)]
 }
 
