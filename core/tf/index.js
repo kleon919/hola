9cd1026d907349b540f75a1ae91ef84b1a73596b
Kleanthis Tsesmetzidis <ktsesmetzidis@gmail.com>
2020-01-27 11:47:44 +0200

    Refine ML Model
    - Convert dob to age
    - Find the minutes from the start of the day, based on current time
    - Make choices/answers more friendly

--- core/tf/index.js
+++ core/tf/index.js
@@ -7,14 +7,14 @@ const init = async () => {
 };
 
 const choices = [
-    "Club Sandwich",
-    "Steak",
-    "Burger",
-    "Chef Salad",
-    "Pasta",
-    "French Fries",
-    "Greek Salad",
-    "Boiled Vegetables"
+    "A Club Sandwich is a cool idea",
+    "You should eat a Burger",
+    "A Beef Steak is the better choice",
+    "You should eat a Chef Salad",
+    "You should have some Pasta",
+    "French Fries is the better choice",
+    "Roasted Vegetables seems like a good option",
+    "You can eat a Greek Salad"
 ];
 
 const normalize = (value, min, max) => {
@@ -26,25 +26,36 @@ const normalize = (value, min, max) => {
 
 };
 
-const normalizeAge = age => normalize(age, 15, 75);
+const toAge = dob => {
+    let ageDate = new Date(Date.now() - new Date(dob).getTime());
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+};
 
-const normalizeTime = secs => normalize(secs, 720, 1320);
+const normalizeAge = dob => normalize(toAge(dob), 15, 75);
 
-const normalizeVegeterian = value => normalize(value, 1, 10);
+const minutes = () => {
+    let dt = new Date();
+    let f = (new Date(Date.UTC(dt.getFullYear(), dt.getMonth(), dt.getDate()))).getTime();
+    return (Date.now() - f) / 60000;
+};
+
+const normalizeTime = () => normalize(minutes(), 720, 1320);
+
+const normalizeVegetarian = value => normalize(value, 1, 10);
 
 const maxIndex = arr => arr.indexOf(Math.max(...arr));
 
-// input : Array [3], [AGE, VEGETERIAN, CURRENT_TIME]
+// input : Array [3], [AGE, VEGETARIAN, CURRENT_TIME]
 const predict = async input => {
 
     let normalInput = [
         normalizeAge(input[0]),
-        normalizeVegeterian(input[1]),
-        normalizeTime(input[2])
+        normalizeVegetarian(input[1]),
+        normalizeTime()
     ];
 
     let res = await model.predict(tf.tensor([normalInput])).array();
     return choices[maxIndex(...res)]
-}
+};
 
 module.exports = {init, predict};
